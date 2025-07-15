127b80b64541ddb3ac4c9e527a04387ae10717f8
taylorwalton <taylor.walton@socfortress.co>
2025-07-15 12:45:48 -0500

    Update agent invocation input for clarity in testing the piHole agent

--- velociraptor_integration.py
+++ velociraptor_integration.py
@@ -42,7 +42,7 @@ async def main():
     print("\n=== Testing Get Agents ===")
     await agent.ainvoke(
         {
-            "input": "Collect the Linux.Sys.Users of the piHole agent.",
+            "input": "What is installed on the piHole agent?",
         },
     )
 
